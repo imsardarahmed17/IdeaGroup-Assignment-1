bc84480a2862e18c6ecc2422a76f40fce2b93e2f
Sardar Ahmed <imsardarahmed@gmail.com>
2022-08-15 11:30:14 +0500

    Added appbar component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 // import logo from './logo.svg';
 import './App.css';
-
+import DenseAppBar from './components/AppBar';
 function App() {
   return (
     <div className="App">
+      <DenseAppBar/>
     </div>
   );
 }
